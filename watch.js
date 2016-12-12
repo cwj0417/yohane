e9665fa040f1b1c059bad7dd63a31b46336cd618
chenwj <731028571@qq.com>
2016-12-12 11:36:32 +0800

    fix not build when inited

--- watch.js
+++ watch.js
@@ -3,6 +3,7 @@ let handlefile = require('./src/rawhandl');
 let config = require('./src/config');
 let path = require('path');
 let root = path.join(__dirname);
+handlefile();
 watch.createMonitor(path.join(root, config.root), function (monitor) {
     monitor.files[path.join(root, config.rawFile)];
     monitor.on("changed", function () {
