233c9f9e6e37a557a92a2f819614c50cab816c1e
chenwj <731028571@qq.com>
2017-04-19 16:31:39 +0800

    fix bug of watch function should be excuted at first time

--- watch.js
+++ watch.js
@@ -7,6 +7,9 @@ module.exports = {
         if (!fs.existsSync(file)) {
             console.log(`entry file not exist ${file}`);
             fs.writeFileSync(target, `export default ["# sorry, ppt source was not found"]`);
+        } else {
+            let raw = fs.readFileSync(file);
+            fs.writeFileSync(target, `export default ${JSON.stringify(escape(raw).split(`%0A%0A---%0A%0A`))}`);
         }
         let root = path.dirname(file);
         watch.createMonitor(root, function (monitor) {
